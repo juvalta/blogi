0e8be2a1e70fab5f8a9d5f9a34ec9af1fa266965
juvalta <62938153+juvalta@users.noreply.github.com>
2025-11-16 22:41:31 +0200

    Added test audio and credits for audio files

--- src/componenets/Card.jsx
+++ src/componenets/Card.jsx
@@ -1,24 +1,43 @@
 import React from "react";
 import "./Card.css"
 import Rating from '@mui/material/Rating'
+import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import { IconButton } from "@mui/material";
+import {useState, useEffect} from "react";
 
 export const Card = ({
     imgSrc,
     imgAlt,
     bugName,
+    audio,
     description,
     buttonText,
     link,
 }) => {
+
+    function play () {
+        new Audio(audio).play()
+    }
+
+    const [value, setValue] = useState(0);
+
+    useEffect(()=>{
+        play()
+    }, [value])
+
     return (
         <div className="bug-card">
             {imgSrc && (
-                <img src ={imgSrc} className="bug-img" />
+                <img src={imgSrc} className="bug-img" />
             )}
             {bugName && <h1 className="bug-name">{bugName}</h1>}
+            {audio &&
+                <IconButton onClick={()=>setValue(value+1)}>
+                    <VolumeUpIcon />
+                </IconButton>}
             {description && <p className="bug-description">{description}</p>}
             <div className="bug-rating">
-            <Rating />
+                <Rating />
             </div>
 
         </div>
--- src/componenets/TokkaaOtokkaa.jsx
+++ src/componenets/TokkaaOtokkaa.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "./header";
-import {Card} from "./Card";
+import { Card } from "./Card";
+import dragonFlySound from "../assets/dragonflywav.wav";
 
-function TokkaaOtokkaa () {
+function TokkaaOtokkaa() {
     return (
         <>
-            <Header/>
+            <Header />
             <Card
                 imgSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/BIEDRONA.JPG/960px-BIEDRONA.JPG"
                 bugName="Leppäkerttu"
@@ -17,6 +18,15 @@ function TokkaaOtokkaa () {
                 bugName="Ketokultasiipi"
                 description="Ketokultasiipi (Lycaena hippothoe) on sinisiipisten heimoon kuuluva pienikokoinen päiväperhonen. Laji esiintyy palearktisella alueella, mutta laji on kuitenkin vahvasti taantunut."
             />
+
+            <Card
+                imgSrc="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Nehalennia_speciosa_female_andreas_thomas_hein_IMG_9209.JPG/1920px-Nehalennia_speciosa_female_andreas_thomas_hein_IMG_9209.JPG"
+                bugName="Kääpiötytönkorento"
+                audio={dragonFlySound}
+                description="Kääpiötytönkorento (Nehalennia speciosa) on hyvin harvinainen sudenkorentolaji. Suomen lajiston uhanalaisarvioinnissa vuodelta 2010 se luokiteltiin erittäin uhanalaiseksi. Se kuuluu Euroopan uhanalaisimpiin sudenkorentolajeihin ja on kansainvälisestikin vaarantunut."
+            />
+
+
         </>
     )
 }
